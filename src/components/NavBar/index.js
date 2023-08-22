8b587679e7b9b4f1c7b636a1b4ee34256b7e1e53
wgm22ar <wgm22ar@gmail.com>
2023-08-22 19:41:28 -0300

    modificacion de nav

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,22 +1,86 @@
 import CartWidget from "../CartWidget";
 const NavBar = () => {
   return (
-    <nav>
-      <h1> HC BEBIDAS </h1>
-      <h3>Tu Tienda de Bebidas Preferida !! </h3>
-      <div>
-        <ul>
-          <h3>Nuestros Productos</h3>
-          <li>
-            <button>Gaseosas</button>
-            <button>Cervezas</button>
-            <button>Bebidas Blancas</button>
-            <button>Contacto</button>
-          </li>
-        </ul>
-      </div>
+    <div>
+      <header>
+        <h1> HC BEBIDAS </h1>
+        <h3>Tu Tienda de Bebidas Preferida !! </h3>
+      </header>
+      <nav class="d-flex flex-row justify-content-around ">
+        <div class="dropdown">
+          <button
+            class="btn btn-primary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Acerca de Nosotros
+          </button>
+          <ul class="dropdown-menu">
+            <li>
+              <button class="dropdown-item" type="button">
+                Quienes Somos
+              </button>
+            </li>
+            <li>
+              <button class="dropdown-item" type="button">
+                Trabaja con Nosotros
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div class="dropdown">
+          <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Nuestros Productos
+          </button>
+          <ul class="dropdown-menu">
+            <li>
+              <button class="dropdown-item" type="button">
+                Cervezas
+              </button>
+            </li>
+            <li>
+              <button class="dropdown-item" type="button">
+                Bebidas Blancas
+              </button>
+            </li>
+            <li>
+              <button class="dropdown-item" type="button">
+                Gaseosas
+              </button>
+            </li>
+            <li>
+              <button class="dropdown-item" type="button">
+                Aguas
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div class="dropdown">
+          <button
+            class="btn btn-success dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Contactanos
+          </button>
+          <ul class="dropdown-menu">
+            <li>
+              <button class="dropdown-item" type="button">
+                Llena el formulario
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <CartWidget />
-    </nav>
+    </div>
   );
 };
 export default NavBar;
