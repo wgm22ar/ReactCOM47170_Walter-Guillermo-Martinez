e4f3166bd892542173b3267de35395b67be94f6c
wgm22ar <wgm22ar@gmail.com>
2023-08-22 19:14:50 -0300

    primer pre-entrega

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ListConteiner";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
