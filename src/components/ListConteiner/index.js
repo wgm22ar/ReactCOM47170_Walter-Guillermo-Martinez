88a959e8d99626461386bf72ef81424e45b2c0a8
wgm22ar <wgm22ar@gmail.com>
2023-08-23 13:20:10 -0300

    Primer pre-entrega

--- src/components/ListConteiner/index.js
+++ src/components/ListConteiner/index.js
@@ -1,7 +1,11 @@
 const ItemListContainer = ({ mensaje }) => {
   return (
-    <div>
-      <h2>{mensaje}</h2>;
+    <div class="alert alert-primary" role="alert">
+      <h2 class="alert-heading">{mensaje}</h2>
+      You successfully read this important alert message.
+      <a href="#!" class="alert-link">
+        It contains a link and heading.
+      </a>
     </div>
   );
 };
